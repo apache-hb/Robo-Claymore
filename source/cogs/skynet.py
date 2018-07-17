6e01dc0ede235bc55a0bf7a14160c99795138a16
Apache-HB <35050275+Apache-HB@users.noreply.github.com>
2018-07-17 16:43:33 +0100

    used spaces instead of tabs

--- source/cogs/skynet.py
+++ source/cogs/skynet.py
@@ -1,4 +1,3 @@
-import discord
 from discord.ext import commands
 from .store import try_file
 import os
@@ -6,37 +5,37 @@ import time
 import json
 
 class SkyNet:
-	def __init__(self, bot):
-		self.bot = bot
-		self.hidden = True
-		print('Cog {} loaded'.format(self.__class__.__name__))
+    def __init__(self, bot):
+        self.bot = bot
+        self.hidden = True
+        print('Cog {} loaded'.format(self.__class__.__name__))
 
-	async def on_message(self, ctx):
-		if ctx.author.bot:
-			return
+    async def on_message(self, ctx):
+        if ctx.author.bot:
+            return
 
-		if ctx.content.startswith(('!', '&', '/', '*', '$', '#')):
-			return
+        if ctx.content.startswith(('!', '&', '/', '*', '$', '#')):
+            return
 
-		new = ''
-		if ctx.guild is None:
-			new = 'cogs/store/skynet/messages/'
-		else:
-			new = 'cogs/store/skynet/{0.guild.id}/'.format(ctx)
+        new = ''
+        if ctx.guild is None:
+            new = 'cogs/store/skynet/messages/'
+        else:
+            new = 'cogs/store/skynet/{0.guild.id}/'.format(ctx)
 
-		os.makedirs(new, exist_ok = True)
+        os.makedirs(new, exist_ok = True)
 
-		ret = json.load(try_file(new + '{}.json'.format(ctx.channel.id)))
-		ret.append({
-			'content': ctx.content,
-			'meta': {
-				'user': ctx.author.id,
-				'name': ctx.author.name,
-				'time': time.time()
-			}
-		})
-		json.dump(ret, open(new + '{}.json'.format(ctx.channel.id), 'w'), indent = 4)
+        ret = json.load(try_file(new + '{}.json'.format(ctx.channel.id)))
+        ret.append({
+            'content': ctx.content,
+            'meta': {
+                'user': ctx.author.id,
+                'name': ctx.author.name,
+                'time': time.time()
+            }
+        })
+        json.dump(ret, open(new + '{}.json'.format(ctx.channel.id), 'w'), indent = 4)
 
 
 def setup(bot):
-	bot.add_cog(SkyNet(bot))
+    bot.add_cog(SkyNet(bot))
