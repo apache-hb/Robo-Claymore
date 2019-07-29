210bfd2b5bb1f20cc8ae59b78b8cf4e7f7fdf375
Elliot Haisley <apachehaisley@gmail.com>
2019-07-29 19:42:27 -0400

    whoops i uploaded a bot token didnt i

--- source/main.py
+++ source/main.py
@@ -6,13 +6,18 @@ from os.path import join, sep
 
 from claymore import Claymore
 
+def get_config():
+    return json.load(open(join('data', 'config.json'), 'r'))
+
 def main(args: List[str]) -> None:
     bot = Claymore()
     
     for path in glob(join('cogs', '*.py')):
         bot.load_extension(path.replace(sep, '.').replace('.py', ''))
     
-    bot.run('NDQ3OTIwOTk4NjIxMjQ5NTM2.XSawZA.24b0hnizTLIM780S2KIhSyvnHvs')
+    config = get_config()
+
+    bot.run(config['discord']['token'])
 
 if __name__ == "__main__":
     main(sys.argv)
