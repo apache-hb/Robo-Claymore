c8eec19f28c4bb6479de4b4a0cc90f92dd444092
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2019-08-11 14:45:39 -0400

    make stuff a bit prettier

--- source/web/src/App.js
+++ source/web/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="app-body">
-      <header className="app-header">
-        <div className="header-item">AAA</div>
-        <div className="header-item">BBB</div>
-      </header>
+    <div>
+      <div className="header">
+        <a href="#top" className="logo">Robo-Claymore</a>
+        <div className="header-right">
+          <AnchorLink href="#features">Features</AnchorLink>
+          <AnchorLink href="#commands">Commands</AnchorLink>
+          <AnchorLink href="#code">Code</AnchorLink>
+          <AnchorLink href="#login">Login</AnchorLink>
+        </div>
+      </div>
+      <div className="content">
+        <a id="top" className="thing">Top</a>
+        <a id="features" className="thing">Features</a>
+        <a id="commands" className="thing">Commands</a>
+        <a id="code" className="thing">Code</a>
+        <a id="login" className="thing">Login</a>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
