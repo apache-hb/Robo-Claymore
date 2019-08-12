55dec659574e39bf2a8a1dc172f1e5ad7a977666
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2019-08-11 22:18:11 -0400

    add some more website stuff

--- source/web/src/App.js
+++ source/web/src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div>
       <div className="header">
-        <a href="#top" className="logo">Robo-Claymore</a>
+        <a href="#" className="logo">Robo-Claymore</a>
         <div className="header-right">
           <AnchorLink href="#features">Features</AnchorLink>
           <AnchorLink href="#commands">Commands</AnchorLink>
@@ -15,11 +15,44 @@ export default function App() {
         </div>
       </div>
       <div className="content">
-        <a id="top" className="thing">Top</a>
-        <a id="features" className="thing">Features</a>
-        <a id="commands" className="thing">Commands</a>
-        <a id="code" className="thing">Code</a>
-        <a id="login" className="thing">Login</a>
+        <div className="top-section">
+          <div className="top-header">
+            <div className="top-title">
+              <a className="invite-button" target="_blank" href="https://discordapp.com/oauth2/authorize?client_id=447920998621249536&amp;scope=bot&amp;permissions=66321471">Invite Claymore</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="section">
+          <a id="features"/>
+          Features
+          <ul>
+            <li>Autorole</li>
+            <li>Tags &amp; quotes</li>
+            <li>Moderation</li>
+            <li>Music player</li>
+            <li>Custom commands</li>
+          </ul>
+        </div>
+
+        <div className="section">
+          <a id="commands"/>
+          Commands
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+
+        <div className="section">
+          <a id="code"/>
+          Github
+          <a className="invite-button" target="_blank" href="https://github.com/Apache-HB/Robo-Claymore">Source code</a>
+        </div>
+
+        <div className="section">
+          <a id="login"/>
+          Login
+        </div>
       </div>
     </div>
   )
