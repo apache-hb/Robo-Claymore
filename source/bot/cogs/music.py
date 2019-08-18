299abc3fce159f067f42294b960e5e3a17af792e
Elliot Haisley <apachehaisley@gmail.com>
2019-08-18 01:44:51 -0400

    add some basic music stuff

--- source/bot/cogs/music.py
+++ source/bot/cogs/music.py
@@ -14,9 +14,42 @@ class Music(Wheel):
             self.bot,
             host='localhost',
             password='lavalink_claymore',
-            rest_port=2332,
+            rest_port=2333,
             ws_port=2333
         )
+        self.bot.log.info('Initialized lavalink connection')
+
+    @commands.command(
+        name = 'join', 
+        aliases = [ 'summon' ]
+    )
+    @commands.guild_only()
+    async def _join(self, ctx):
+        pass
+
+    @commands.command(name = 'play')
+    @commands.guild_only()
+    async def _play(self, ctx, *, song: str):
+        player = await lavalink.connect(ctx.author.voice.channel)
+        tracks = await player.search_yt(song)
+        player.add(ctx.author, tracks.tracks[0])
+        await player.play()
+
+    @commands.command(name = 'skip')
+    @commands.guild_only()
+    async def _skip(self, ctx, count: int = 1):
+        pass
+
+    @commands.command(name = 'queue')
+    @commands.guild_only()
+    async def _queue(self, ctx):
+        pass
+
+    @commands.command(name = 'leave')
+    @commands.guild_only()
+    async def _leave(self, ctx):
+        player = lavalink.get_player(ctx.guild.id)
+        await player.disconnect()
 
     def cog_unload(self):
         loop = asyncio.get_event_loop()
