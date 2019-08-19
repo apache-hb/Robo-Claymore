ab269efa0e58c64d72c8d20badcb2fbc3abacbb1
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2019-08-18 22:03:55 -0400

    add back some more commands

--- source/bot/cogs/fun.py
+++ source/bot/cogs/fun.py
@@ -2,7 +2,9 @@ from claymore import Wheel
 import discord
 from discord.ext import commands
 import random
+from utils import json, can_embed
 from random import choice, randint
+from pyfiglet import figlet_format
 
 BALL_OPTIONS = [
     'Yes',
@@ -34,6 +36,73 @@ class Fun(Wheel):
     @commands.command(name = 'reverse')
     async def _reverse(self, ctx, *, text: str):
         await ctx.send(text[::-1])
+
+    @commands.command(name = 'hash')
+    async def _hash(self, ctx, *, text: str):
+        await ctx.send(hash(text))
+
+    @commands.command(name = 'expand')
+    async def _expand(self, ctx, *, text: str = 'dong'):
+        ret = figlet_format(text, font = choice(['big', 'contessa', '5lineoblique', 'alphabet', 'banner', 'doom']))
+
+        if len(ret) < 1900:
+            return await ctx.send('Text is too big to send through discord')
+
+        await ctx.send(f'```{ret}```')
+
+    @commands.command(name = 'wolfram')
+    async def _wolfram(self, ctx, *, query: str):
+        pass
+
+    @commands.command(name = 'reddit')
+    async def _reddit(self, ctx, sub: str = 'all', search: str = 'new', index: int = None):
+        if index is not None and 0 <= index <= 25:
+            return await ctx.send('Index must be between 0 and 25')
+
+        search = search.lower()
+        if 'n' in search:
+            search = 'new'
+        elif 'h' in search:
+            search = 'hot'
+        elif 't' in search:
+            search = 'top'
+        else:
+            return await ctx.send('Search must be new, hot or top')
+
+        url = f'https://www.reddit.com/r/{sub.lower()}/{search}.json?t=all'
+
+        dat = await json(url)
+
+        if dat.get('error', False):
+            return await ctx.send(f'`{sub}` is a private subreddit')
+
+        if not dat['data']['children']:
+            return await ctx.send(f'Could not find `{sub}`')
+
+        if index is None:
+            post = choice(dat['data']['children'])
+        else:
+            try:
+                post = dat['data']['children'][index]
+            except IndexError:
+                return await ctx.send(f'There is no post at an index of `{index}`')
+
+        if post['data']['over_18'] and not ctx.channel.is_nsfw():
+            return await ctx.send('That post is nsfw, and must be requested in an nsfw channel')
+
+        data = post['data']
+
+        embed = ctx.make_embed(f'Post from {sub}', f'Posted by {data["author"]}')
+        embed.add_field(name = 'Link', value = data['url'])
+        embed.add_field(name = 'Votes', value = f'{data["ups"]} upvotes & {data["downs"]} downvotes')
+
+        if data['selftext']:
+            embed.add_field(name = 'Selftext', value = data['selftext'][:250] + (data['selftext'][250:] and '...'))
+
+        if can_embed(data['url']):
+            embed.set_image(url = data['url'])
+
+        await ctx.send(embed = embed)
 
     @commands.command(name = '8ball')
     async def _8ball(self, ctx, *, thing: str):
