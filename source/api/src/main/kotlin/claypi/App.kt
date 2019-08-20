915193c05cc45cf08e7ddcc8c2ae6e13635e425d
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2019-08-20 14:58:28 -0400

    oh boy more backend stuff

--- source/api/src/main/kotlin/claypi/App.kt
+++ source/api/src/main/kotlin/claypi/App.kt
@@ -17,6 +17,9 @@ import com.serebit.strife.*
 
 // oauth discord stuff
 import org.jetbrains.hub.oauth2.client.*
+import java.net.URLEncoder
+import java.net.URL
+import io.github.rybalkinsd.kohttp.dsl.*
 
 // coroutines
 import kotlinx.coroutines.*
@@ -25,6 +28,7 @@ import kotlinx.coroutines.*
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
+import io.ktor.features.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
@@ -33,9 +37,9 @@ fun config(path: String) = Wini(File(path))
 
 fun Wini.getInt(head: String, field: String): Int? = this.get(head, field, Int::class.java)
 
-const val API_VERSION = "/api/v1"
-
 fun String.toJsonString(): String = "\"$this\""
+
+class NotFound : Exception()
 
 suspend fun main(args: Array<String>) {
     val cfg = config("../config/config.ini")
@@ -44,9 +48,15 @@ suspend fun main(args: Array<String>) {
     val client = if(mongoUrl != null) MongoClients.create(mongoUrl) else MongoClients.create()
     val db = client.getDatabase(cfg.get("mongo", "name"))
     val prefixes = db.getCollection("prefix")
+
     val defaultPrefix = cfg.get("discord", "prefix")
 
+    val discordID = cfg.getInt("oauth", "id")!!
+    val discordSecret = cfg.get("oauth", "secret")
+
     val port = cfg.getInt("api", "port")!!
+    val discordCreds = Base64.encode("$discordID:$discordSecret".toByteArray())
+    val redirect = URLEncoder.encode("http://localhost:$port/api/discord/callback", "UTF-8")
 
     var discord: BotClient? = null
 
@@ -60,8 +70,11 @@ suspend fun main(args: Array<String>) {
     }
 
     embeddedServer(Netty, port) {
+        install(StatusPages) {
+            exception<NotFound> { _ -> call.respond(HttpStatusCode.NotFound) }
+        }
         routing {
-            route(API_VERSION) {
+            route("api/v1") {
                 // get the prefix for a server
                 get("prefix") {
                     when(val id = call.parameters["id"]?.toLongOrNull()) {
@@ -75,10 +88,36 @@ suspend fun main(args: Array<String>) {
                 get("prefix/global") {
                     call.respondText("""{ "prefix": "$defaultPrefix" }""", ContentType.Application.Json)
                 }
-            }
-            route("discord") {
-                get("callback") {
 
+                get("bot") {
+
+                }
+            }
+
+            route("discord") {
+                get("login") {
+                   call.respondRedirect("https://discordapp.com/api/oauth2/authorize?client_id=$discordID&scope=identify&response_type=code&redirect_uri=$redirect")
+                }
+                get("callback") {
+                    call.parameters["code"]?.let { code ->
+                        httpPost {
+                            host = "https://discordapp.com"
+                            path = "/api/oauth2/token"
+
+                            param {
+                                "grant_type" to "authorization_code"
+                                "code" to code
+                                "redirect_uri" to redirect
+                            }
+
+                            header {
+                                "Authorization" to "Basic $discordCreds"
+                            }
+                        }
+                    }?.use { data ->
+                        if(data.code() != 200)
+                            throw NotFound()
+                    }
                 }
             }
         }
