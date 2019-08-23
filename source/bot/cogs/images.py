d84c0edea84e79a9c72e40deb68b169a1994475e
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2019-08-22 22:52:09 -0400

    change some code update gitignore

--- source/bot/cogs/images.py
+++ source/bot/cogs/images.py
@@ -13,25 +13,31 @@ class Images(Wheel):
         super().__init__(bot)
 
         images_path = join('..', 'data', 'images', '*.*')
-        self.images = {splitext(basename(img))[0]:Image.open(img).convert('ARGB') for img in glob(images_path)}
+        self.images = {splitext(basename(img))[0]:Image.open(img).convert('RGBA') for img in glob(images_path)}
 
         vince_path = join('..', 'data', 'images', 'vince', '*.*')
-        self.vince = {splitext(basename(img))[0]:Image.open(img).convert('ARGB') for img in glob(vince_path)}
-        self.vince_range = range(2, max([name[0] for name, _ in self.vince.items()]))
+        self.vince = {splitext(basename(img))[0]:Image.open(img).convert('RGBA') for img in glob(vince_path)}
+        self.vince_range = range(2, max([int(name[0]) for name, _ in self.vince.items()]))
 
         brains_path = join('..', 'data', 'images', 'brain', '*.*')
-        self.brain = {splitext(basename(img))[0]:Image.open(img).convert('ARGB') for img in glob(brains_path)}
-        self.brain_range = range(2, max([name[0] for name, _ in self.brain.items()]))
+        self.brain = {splitext(basename(img))[0]:Image.open(img).convert('RGBA') for img in glob(brains_path)}
+        self.brain_range = range(2, max([int(name[0]) for name, _ in self.brain.items()]))
 
     @commands.command(name = 'vince')
     @commands.cooldown(1, 15, commands.BucketType.user)
-    def _vince(self, ctx, *, fields: str):
-        pass
+    async def _vince(self, ctx, *, fields: str):
+        parts = fields.split('|')
+
+        if len(parts) not in self.vince_range:
+            return await ctx.send(f'There must be between `{self.vince_range.start}` and `{self.vince_range.stop}` fields')
 
     @commands.command(name = 'brain')
     @commands.cooldown(1, 15, commands.BucketType.user)
-    def _brain(self, ctx, *, fields: str):
-        pass
+    async def _brain(self, ctx, *, fields: str):
+        parts = fields.split('|')
+
+        if len(parts) not in self.brain_range:
+            return await ctx.send(f'There must be between `{self.brain_range.start}` and `{self.brain_range.stop}` fields')
 
 def setup(bot):
     bot.add_cog(Images(bot))
