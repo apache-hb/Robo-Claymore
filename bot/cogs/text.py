3febf08a24eab1747e74fd19bcefa31e77bb78c2
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-11-07 15:47:53 -0500

    autoreacts

--- bot/cogs/text.py
+++ bot/cogs/text.py
@@ -5,16 +5,39 @@ from claymore.utils import wheel
 from discord.ext.commands import command
 
 class Text(wheel(desc = 'automatic messages')):
+    def __init__(self, bot):
+        super().__init__(bot)
+
+        @bot.listen()
+        async def on_message(message):
+            if message.author.bot:
+                return
+
+            reacts = self.db.reacts.find_one({ 'id': message.guild.id })
+
+            for text, emotes in (reacts or {}).items():
+                if 'id' in text:
+                    continue
+
+                if text in message.content:
+                    for emote in emotes:
+                        await message.add_reaction(emote)
+
     @group()
     async def reacts(self, ctx):
         pass
 
     @reacts.command()
-    async def add(self, ctx, emote: Emoji, *, text: str):
-        pass
+    async def add(self, ctx, emote: str, *, text: str):
+        self.db.reacts.update(
+            { 'id': ctx.guild.id },
+            { '$set': { 'id': ctx.guild.id }, '$addToSet': { text: emote } },
+            upsert = True
+        )
+        await ctx.send(f'will now react to `{text}` with `{emote}`')
 
     @reacts.command()
-    async def remove(self, ctx, *, it: Union[Emoji, str]):
+    async def remove(self, ctx, *, it: str):
         pass
 
     @group()
